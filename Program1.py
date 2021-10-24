c58848a4c02de221cf18b3bdef862a87bb139789
Magno <clarenceamagno@iskolarngbayan.pup.edu.ph>
2021-10-24 08:59:52 +0800

    third commit

--- Program1.py
+++ Program1.py
@@ -3,4 +3,4 @@ name = input("What is your name?: ")
 age = input ("What is your age?: ") 
 address = input("What is your address?: ")
 
-print("Hi, my name is " + name + ". I am " + age + " years old and I live in " + address + "\n")
+print("Hi, my name is " + name + ". I am " + age + " years old and I live in " + address)
